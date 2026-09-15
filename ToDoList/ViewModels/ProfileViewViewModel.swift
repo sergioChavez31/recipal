ee4ffe232660789452fe775c72007b97cbd0a38d
sergMaybeCodes31 <schavez31@gatech.edu>
2026-09-15 01:20:52 -0400

    added ability for users to see the tasks they have added

--- ToDoList/ViewModels/ProfileViewViewModel.swift
+++ ToDoList/ViewModels/ProfileViewViewModel.swift
@@ -8,4 +8,8 @@
 import Foundation
 class ProfileViewViewModel: ObservableObject {
     init() {}
+    
+    func toggleIsDone(item: ToDoListItem) {
+        
+    }
 }
--- ToDoList/Views/ToDoListView.swift
+++ ToDoList/Views/ToDoListView.swift
@@ -4,22 +4,34 @@
 //
 //  Created by Sergio Chavez on 9/14/26.
 //
-
+import FirebaseFirestore
 import SwiftUI
 
 struct ToDoListView: View {
     @StateObject var viewModel = ToDoListViewViewModel()
+    @FirestoreQuery var items: [ToDoListItem]
     
     private let userId: String
     
     init(userId: String) {
         self.userId = userId
+        self._items = FirestoreQuery(
+            collectionPath: "users/\(userId)/todos"
+        )
     }
     
     var body: some View {
         NavigationView {
             VStack {
-                
+                List(items) { item in
+                    ToDoListItemView(item: item)
+                        .swipeActions {
+                            Button("delete") {
+                                viewModel.delete(id: item.id)
+                            }
+                            .foregroundColor(Color.red)
+                        }
+                }
             }
             .navigationTitle("To Do List")
             .toolbar {
@@ -38,5 +50,5 @@ struct ToDoListView: View {
 }
 
 #Preview {
-    ToDoListView(userId: "")
+    ToDoListView(userId: "vwZytp65CThOfjgaPbcWpRWL9kB3")
 }
